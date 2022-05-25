30e33d2b3e458bf31ec12abc266fda56e5f9a563
Kevin Xiao <kevinxiao@bituss-air.lan>
2022-05-24 19:40:04 -0700

    added button functionality and animation

--- frontend/src/components/createOverlay.js
+++ frontend/src/components/createOverlay.js
@@ -2,14 +2,12 @@
 
 function CreatePostOverlay( {show} ) {
     if (show) {
-        console.log("overlay shown")
         return(
             <div className="overlay">
                 bruh
             </div>
         );
     } else {
-        console.log("overlay hidden");
         return;
     }
 }
