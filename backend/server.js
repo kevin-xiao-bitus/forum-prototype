85c7d66f061b275c1fcc0e7d2d5ff59cc30f92dc
Kevin Xiao <kevinxiao@Bituss-MacBook-Air.local>
2022-06-08 14:29:53 -0700

    restructured post data structure

--- backend/server.js
+++ backend/server.js
@@ -10,57 +10,87 @@ const fs = require("fs");
 server.use(bodyParser.json());
 server.use(cors());
 
+const previewLength = 1500;
+
+function getPostById(id, preview = false) {
+    const dir = "./posts/" + id;
+    var post = JSON.parse(fs.readFileSync(dir + "/post.json", {
+        encoding: "utf-8"
+    }));
+    const buffer = fs.readFileSync(dir + "/content.txt", {
+        encoding: "utf-8"
+    });
+    post.content = (preview) ? buffer.substring(0, previewLength) : buffer;
+    if (!preview) {
+        post.replies = JSON.parse(fs.readFileSync(dir + "/replies.json", {
+            encoding: "utf-8"
+        })).replies;
+    }
+    return post;
+}
+
+//console.log(getPostById(0, false))
+
 server.post("/getPosts", (req, res) => {
     const query = req.body.keyword;
-    const posts = directory.Posts;
+    let posts = [];
     if (query === "") {
-        res.send(JSON.stringify(directory));
+        directory.Posts.forEach((id) => {
+            posts.push(getPostById(id, true));
+        })
+        res.send(JSON.stringify(posts));
     }
     else {
-        let results = {Posts: []};
-        posts.forEach((post) => {
+        directory.Posts.forEach((id) => {
+            let post = getPostById(id, false);
             let titleMatch = post.title.toLowerCase().indexOf(query);
             let contentMatch = post.content.toLowerCase().indexOf(query);
             let posterMatch = post.poster.toLowerCase().indexOf(query);
             if (!((titleMatch === -1) && (contentMatch === -1) && (posterMatch === -1))) {
-                results.Posts.push(post);
+                delete post.replies;
+                post.content = post.content.substring(0, previewLength);
+                posts.push(post);
             }
         });
-        const responseData = JSON.stringify(results);
+        const responseData = JSON.stringify(posts);
         res.send(responseData);
     }
 });
 
-const previewLength = 1500;
-
 server.post("/uploadPost", (req, res) => {
-    const data = req.body;
+    let data = req.body;
     data.id = directory.idCount++;
-    let previewPost = {...data};
-    if (previewPost.content.length > previewLength) {
-        previewPost.content = previewPost.content.substring(0, previewLength);
-    }
-    directory.Posts.push(previewPost);
+    const fullContent = data.content;
 
-    let fullPost = {...data};
-    fullPost.replies = [];
+    delete data.content;
+    let post = {...data};
+    let replies = {replies: []};
 
+    directory.Posts.push(data.id);
     async function setupFiles () {
+        const dir = "./posts/" + data.id;
         fs.writeFileSync("./directory.json", JSON.stringify(directory), e => {
-            if (e) console.log(e)});
-        fs.mkdirSync(`./posts/${data.id}`, e => {
-            if (e) console.log(e)});
-        fs.writeFileSync(`./posts/${data.id}/post.json`, JSON.stringify(fullPost), e => {
-            if (e) console.log(e)});
+            if (e) console.log(e)
+        });
+        fs.mkdirSync(dir, e => {
+            if (e) console.log(e)
+        });
+        fs.writeFileSync(dir + "/post.json", JSON.stringify(post), e => {
+            if (e) console.log(e)
+        });
+        fs.writeFileSync(dir + "/content.txt", fullContent, e => {
+            if (e) console.log(e);
+        })
+        fs.writeFileSync(dir + "/replies.json", JSON.stringify(replies), e => {
+            if (e) console.log(e);
+        });
     }
     setupFiles();
 })
 
 server.post("/getPostById", (req, res) => {
     const id = req.body.id;
-    const post = fs.readFileSync(`./posts/${id}/post.json`, {
-        encoding: "utf-8"
-    });
+    const post = getPostById(id, false);
     if (post) {
         const responseData = post;
         res.send(responseData);
